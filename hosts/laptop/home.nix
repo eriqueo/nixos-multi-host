2ca7806ee9de4feb74e8e0d3f642e05b85170995
eric <eriqueo@proton.me>
2025-06-11 10:17:16 -0600

    home tweaks

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -31,17 +31,70 @@ in
     ollama
   ];
 	wayland.windowManager.hyprland = {
-		enable = true;
-		extraConfig = ''
-		$mod = SUPER
-		bind = $mod, Return, exec, kitty
-		bind = $mod, Q, killactive
-		bind = $mod, F, fullscreen
-		bind = $mod, Space, exec, wofi --show drun
-		bind = , Print, exec, hyprshot -m region -o ~/Pictures/Screenshots
-		bind = SHIFT, Print, exec, hyprshot -m region -c
-
-		'';
+		  enable = true;
+		  extraConfig = ''
+		    # Mouse settings
+		    input {
+		      kb_layout = us
+		      follow_mouse = 1
+		      touchpad {
+		        natural_scroll = true
+		      }
+		    }
+		    
+		    # Monitor setup
+		    monitor = eDP-1, 2560x1600@165, 1920x0, 1.6
+		    
+		    # Startup applications
+		    exec-once = waybar
+		    
+		    # Variables
+		    $mod = SUPER
+		    
+		    # Window/Session Management
+		    bind = $mod, Return, exec, kitty
+		    bind = $mod, Q, killactive
+		    bind = $mod, F, fullscreen
+		    bind = $mod, Space, exec, wofi --show drun
+		    bind = $mod, B, exec, librewolf
+		    bind = $mod, E, exec, electronmail
+		    
+		    # Screenshots
+		    bind = , Print, exec, hyprshot -m region -o ~/Pictures/Screenshots
+		    bind = SHIFT, Print, exec, hyprshot -m region -c
+		    
+		    # Focus movement (SUPER + arrows)
+		    bind = $mod, left, movefocus, l
+		    bind = $mod, right, movefocus, r
+		    bind = $mod, up, movefocus, u
+		    bind = $mod, down, movefocus, d
+		    
+		    # Window movement within workspace (SUPER + ALT + arrows)
+		    bind = $mod ALT, left, movewindow, l
+		    bind = $mod ALT, right, movewindow, r
+		    bind = $mod ALT, up, movewindow, u
+		    bind = $mod ALT, down, movewindow, d
+		    
+		    # Move windows to workspaces (SUPER + CTRL + numbers)
+		    bind = $mod CTRL, 1, movetoworkspace, 1
+		    bind = $mod CTRL, 2, movetoworkspace, 2
+		    bind = $mod CTRL, 3, movetoworkspace, 3
+		    bind = $mod CTRL, 4, movetoworkspace, 4
+		    bind = $mod CTRL, 5, movetoworkspace, 5
+		    bind = $mod CTRL, 6, movetoworkspace, 6
+		    bind = $mod CTRL, 7, movetoworkspace, 7
+		    bind = $mod CTRL, 8, movetoworkspace, 8
+		    
+		    # Switch to workspaces (SUPER + CTRL + ALT + numbers)
+		    bind = $mod CTRL ALT, 1, workspace, 1
+		    bind = $mod CTRL ALT, 2, workspace, 2
+		    bind = $mod CTRL ALT, 3, workspace, 3
+		    bind = $mod CTRL ALT, 4, workspace, 4
+		    bind = $mod CTRL ALT, 5, workspace, 5
+		    bind = $mod CTRL ALT, 6, workspace, 6
+		    bind = $mod CTRL ALT, 7, workspace, 7
+		    bind = $mod CTRL ALT, 8, workspace, 8
+		  '';
 		settings = {
 		exec-once = [ "waybar" ];
 		monitor = [ "eDP-1, 2560x1600@165, 1920x0, 1.6" ];
@@ -69,12 +122,51 @@ in
 				modules-left = [ "hyprland/workspaces" ];
 				modules-center = [ "clock" ];
 				modules-right = [ "pulseaudio" "battery" "tray" ];
-				clock.format = "{:%H:%M}";
-				pulseaudio.format = "{volume}% {icon}";
-				battery.format = "{capacity}% {icon}";
-				tray.spacing = 10;
-			};
-		};
+				"hyprland/workspaces" = {
+				        disable-scroll = true;
+				        all-outputs = true;
+				        format = "{name}";
+				        format-icons = {
+				          "1" = "Web";  # Web
+				          "2" = "Email";   # Terminal  
+				          "3" = "JT";  # Music
+				          "4" = "Notes";   # Chat
+				          "5" = "Code";   # Files
+				          "6" = "Media";   # Code
+				          "7" = "Misc";   # Games
+				          "8" = "AI";   # System
+				        };
+				        persistent-workspaces = {
+				          "*" = 8;
+				        };
+				      };
+				      
+				      clock = {
+				        format = "{:%H:%M}";
+				        format-alt = "{:%Y-%m-%d}";
+				      };
+				      
+				      pulseaudio = {
+				        format = "{volume}% {icon}";
+				        format-muted = " ";
+				        format-icons = ["" "" ""];
+				        on-click = "pavucontrol";
+				      };
+				      
+				      battery = {
+				        format = "{capacity}% {icon}";
+				        format-icons = ["" "" "" "" ""];
+				        states = {
+				          warning = 30;
+				          critical = 15;
+				        };
+				      };
+				      
+				      tray = { 
+				        spacing = 10; 
+				      };
+				    };
+				  };
 	
 		style = ''
 		* {
