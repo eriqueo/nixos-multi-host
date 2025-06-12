8db8dedebd3f026718636b3a8a46f43fd07c1f1a
eric <eriqueo@proton.me>
2025-06-12 17:22:07 -0600

    proton

--- modules/ui/stylix.nix
+++ modules/ui/stylix.nix
@@ -1,17 +1,26 @@
-# modules/ui/stylix.nix
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
-  stylix = {
-    enable       = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    fonts = {
-		monospace = { package = pkgs.nerd-fonts.caskaydia-cove; ... };
-	    sans      = { package = pkgs.fira; ... };
-	    emoji     = { package = pkgs.twemoji-color-font; ... };
+let
+  base16 = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+in {
+  config = {
+    stylix = {
+      enable       = true;
+      base16Scheme = base16;
+      fonts = {
+        monospace = {
+          package = pkgs.nerd-fonts.caskaydia-cove;
+          size    = 12;
+        };
+        sans = {
+          package = pkgs.fira;
+          size    = 11;
+        };
+        emoji = {
+          package = pkgs.twemoji-color-font;
+          size    = 13;
+        };
       };
     };
   };
-
-  stylix.targets.firefox.profileNames = [ "3bp09ufp.default" ];
 }
