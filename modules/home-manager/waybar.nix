de24ae4b7ebdf164b99739b11125e4b0e534c9dc
eric <eriqueo@proton.me>
2025-06-19 23:36:55 -0600

    hyprsome yes

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -55,14 +55,6 @@
           active = "";
           default = "";
         };
-        persistent-workspaces = {
-          # External monitor workspaces
-          "1" = []; "2" = []; "3" = []; "4" = [];
-          "5" = []; "6" = []; "7" = []; "8" = [];
-          # Laptop monitor workspaces  
-          "11" = []; "12" = []; "13" = []; "14" = [];
-          "15" = []; "16" = []; "17" = []; "18" = [];
-        };
       };
 
       "hyprland/window" = {
