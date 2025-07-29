e504e773378da221ce795c44c51acdc6e20b2fef
eric <eriqueo@proton.me>
2025-07-29 11:48:55 -0600

    tlp instead of autocpufreq

--- hosts/laptop/config.nix
+++ hosts/laptop/config.nix
@@ -149,7 +149,7 @@
   # 13. POWER MANAGEMENT (laptop-specific)
   ####################################################################
   services.thermald.enable = true;
-  services.auto-cpufreq.enable = true;
+  services.tlp.enable = true;
   powerManagement.enable = true;
   
   ####################################################################
