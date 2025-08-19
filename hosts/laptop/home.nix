ac72c9ffe9ce7af698c8ee499b3a525c1d353a7d
eric <eriqueo@proton.me>
2025-08-19 01:23:35 -0600

    bookmarksfix

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -21,12 +21,29 @@
     ./modules/virtualization.nix
     ./modules/betterbird.nix
   ];
+  # 1) Make HM own the file (source-of-truth in your repo) and always overwrite it.
+  xdg.configFile."gtk-3.0/bookmarks" = {
+    # pick ONE: use text inline OR source a tracked file in your repo
+    # text = ''
+    #   file:///home/eric/Documents
+    #   file:///home/eric/Downloads
+    # '';
+    source = ./gtk/bookmarks;   # <- put a canonical file under hosts/laptop/gtk/bookmarks
+    force  = true;              # <- skip backups and clobber non-symlink targets
+  };
 
+  # 2) One-time cleanup guard so your first switch won’t fail if old backups exist.
+  home.activation.pruneExistingHmBackups =
+    lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+      rm -f "$HOME/.config/gtk-3.0/bookmarks.backup" \
+            "$HOME/.config/gtk-3.0/bookmarks.hm-bak"
+    '';
   # IDENTITY
   home.username = "eric";
   home.homeDirectory = "/home/eric";
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
+
   # SHARED MODULE CONFIGURATIONS
   # The following are imported from shared modules:
   # - Core CLI tools (bat, eza, fzf, ripgrep, btop, micro, tmux, etc.)
