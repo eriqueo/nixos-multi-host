4570b097a99b75e35aa2538ad47930a8f6a7e66f
eric <eriqueo@proton.me>
2025-08-24 14:00:54 -0600

    waybar fix?

--- hosts/laptop/modules/waybar.nix
+++ hosts/laptop/modules/waybar.nix
@@ -1137,25 +1137,6 @@ in
   };
 
   # Clean GTK settings
-  home.file.".config/gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name=Adwaita-dark
-    gtk-icon-theme-name=Adwaita
-    gtk-font-name=Sans 11
-    gtk-cursor-theme-name=Adwaita
-    gtk-cursor-theme-size=24
-    gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
-    gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-    gtk-button-images=0
-    gtk-menu-images=0
-    gtk-enable-event-sounds=1
-    gtk-enable-input-feedback-sounds=0
-    gtk-xft-antialias=1
-    gtk-xft-hinting=1
-    gtk-xft-hintstyle=hintfull
-    gtk-xft-rgba=rgb
-    gtk-application-prefer-dark-theme=1
-  '';
 
   # Enhanced environment variables
   home.sessionVariables = {
